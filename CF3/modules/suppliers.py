a03036584dfd7853d06e72c1c17ee5165862073f
gerrit0492-create <gerrit0492@gmail.com>
2026-05-24 13:22:52 +0200

    build: add supplier intelligence module

---
+++ CF3/modules/suppliers.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def render():
+    st.title('Supplier Intelligence')
+    st.caption('Supplier pricing and sourcing overview')
+
+    st.info('Supplier intelligence module active')
