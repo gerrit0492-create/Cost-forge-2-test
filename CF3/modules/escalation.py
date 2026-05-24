313bf61140cdbf8cad4ea3675d79f35349cdf00a
gerrit0492-create <gerrit0492@gmail.com>
2026-05-24 13:23:09 +0200

    build: add escalation module

---
+++ CF3/modules/escalation.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def render():
+    st.title('Escalation Engine')
+    st.caption('Inflation and commodity escalation modelling')
+
+    st.info('Escalation engine active')
