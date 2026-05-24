48b00e92fc2bca31b8cc72745aee5f5b15d9cb94
gerrit0492-create <gerrit0492@gmail.com>
2026-05-24 21:28:03 +0200

    final: add deployment configuration

---
+++ CF3/config/deployment_config.py
@@ -0,0 +1,7 @@
+APP_ENV = 'production'
+APP_NAME = 'Cost Forge 3 Enterprise'
+STREAMLIT_MAIN = 'CF3/app/main.py'
+DATABASE_MODE = 'sqlite'
+ENABLE_AI = True
+ENABLE_FORECASTING = True
+ENABLE_REPORTING = True
