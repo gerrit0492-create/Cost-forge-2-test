8a880d526c24c25195b21b6e441ff280d3398567
gerrit0492-create <gerrit0492@gmail.com>
2026-05-24 20:47:30 +0200

    final: add enterprise home module

---
+++ CF3/modules/home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+
+def render():
+    st.title('Cost Forge 3 Enterprise')
+    st.caption('Unified Manufacturing Cost Engineering Platform')
+
+    st.success('CF3 Enterprise operational')
+
+    st.markdown('''
+    ### Core Workflows
+    - Projects
+    - BOM Intelligence
+    - Costing
+    - Routing
+    - Escalation
+    - Forecasting
+    - Suppliers
+    - Reporting
+    - Export Center
+    ''')
