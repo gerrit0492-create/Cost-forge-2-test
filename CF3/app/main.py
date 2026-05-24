9757e9e5781f0ba3172e0d76b03a4bd3935f8725
gerrit0492-create <gerrit0492@gmail.com>
2026-05-24 23:11:36 +0200

    Update main.py

--- CF3/app/main.py
+++ CF3/app/main.py
@@ -1,5 +1,16 @@
 from __future__ import annotations
+import os
+import sys
 
+BASE_DIR = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        ".."
+    )
+)
+
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 import importlib
 import streamlit as st
 
