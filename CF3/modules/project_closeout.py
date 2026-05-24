3aa3c39f445c6910f550100bb7fe9194e0274287
gerrit0492-create <gerrit0492@gmail.com>
2026-05-24 18:52:08 +0200

    build: add project closeout module

---
+++ CF3/modules/project_closeout.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def render():
+    st.title('Project Close-out')
+    st.caption('Final project and financial closure')
+
+    st.success('Project close-out workflow active')
