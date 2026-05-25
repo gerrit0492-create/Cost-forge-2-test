39f3949abfaf7871fabf3db03dfeb40ab595e38e
gerrit0492-create <gerrit0492@gmail.com>
2026-05-25 07:26:18 +0200

    build: add home module

---
+++ modules/home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+def render_home():
+    st.title('Cost Forge 2.0')
+    st.subheader('Manufacturing Cost Engineering Platform')
+    st.success('Stable runtime initialized')
